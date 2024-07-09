6f7690f28503b67a2c2f4cd1d81f360783f8d58b
Hitesh-Meghwal <21sycshiteshmeghwal@gmail.com>
2024-07-09 17:57:04 +0530

    data model and entities and repo created

---
+++ lib/features/daily_news/domain/repository/article_repository.dart
@@ -0,0 +1,7 @@
+import 'package:newsapp/core/resources/data_state.dart';
+import 'package:newsapp/features/daily_news/domain/entities/articles.dart';
+
+abstract class ArticleRepository{
+
+  Future<DataState<List<ArticleEntity>>> getNewsArticle();
+}
